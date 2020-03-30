e2d8c9f4030cdac967ce9d43741b49f6f5fd5b1b
Amr Abulseoud <amrabulseoud@gmail.com>
2020-03-30 14:14:18 +0200

    [FEAT]: added resume

--- src/pages/resume.tsx
+++ src/pages/resume.tsx
@@ -4,9 +4,10 @@ import { Link } from "gatsby"
 const ResumePage = () => (
   <div className="bg-gray-900 h-screen w-screen">
     <div className="text-white text-center">
-      Nothing here yet
       <Link className="block underline text-blue-500" to="/">Go back to the homepage</Link>
     </div>
+    <iframe style={{ maxWidth: "9in" }} className="min-h-screen mx-auto w-full"
+            src={process.env.RESUME_PDF_URL} />
   </div>
 )
 
