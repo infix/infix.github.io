04c89bd1d0275954a2ab6cea44fac59b6e383053
Amr Abulseoud <amrabulseoud@gmail.com>
2020-03-30 04:40:51 +0200

    [FEAT]: created initial outline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,12 +3,19 @@ import { Link } from "gatsby"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
-  <div className={"bg-gray-900 h-screen w-screen"}>
+  <div className="bg-gray-900 h-screen w-screen">
     <SEO title="Home" />
 
-    <div className={"mx-auto container text-center text-gray-100"}>
-      <div>Links:</div>
-      <Link to="/page-2/">Go to the other page</Link>
+    <div className="container">
+      <h1 className="text-white text-center text-4xl">
+        Hi, I'm Amr
+      </h1>
+
+      <div className="mx-auto text-center text-gray-100">
+        <Link className="block underline text-blue-500" to="/resume/">Resume</Link>
+        <Link className="block underline text-blue-500" to="/resume/">Skills</Link>
+        <Link className="block underline text-blue-500" to="/resume/">Projects</Link>
+      </div>
     </div>
   </div>
 )
---
+++ src/pages/resume.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Link } from "gatsby"
+
+const ResumePage = () => (
+  <div className="bg-gray-900 h-screen w-screen">
+    <div className="text-white text-center">
+      Nothing here yet
+      <Link className="block underline text-blue-500" to="/">Go back to the homepage</Link>
+    </div>
+  </div>
+)
+
+export default ResumePage
