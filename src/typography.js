eab5db7597282ecf1b5886cb816757bc470a8ef5
Amr Abulseoud <amrabulseoud@gmail.com>
2020-04-01 11:50:31 +0200

    Updates

--- src/typography.js
+++
@@ -1,9 +0,0 @@
-import Typography from "typography"
-
-export default new Typography({
-  baseFontSize: "18px",
-  baseLineHeight: 1.666,
-  headerFontFamily: ["Ubuntu", "san-serif"],
-  bodyFontFamily: ["Ubuntu", "san-serif"],
-})
-
---
+++ styles-7c9bb8bd07918d9f37a1.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{"20M+":function(n,o,i){},"2aeQ":function(n,o,i){},"5itS":function(n,o,i){}}]);
+//# sourceMappingURL=styles-7c9bb8bd07918d9f37a1.js.map
