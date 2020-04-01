f163eb3e21e1c97a4e506d5abba177550002be16
Amr Abulseoud <amrabulseoud@gmail.com>
2020-04-01 03:18:59 +0200

    [FEAT]: moved prettify button to bottom right corner of the editor

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useEffect, useRef } from "react"
 // @ts-ignore
 import GraphiQL from "graphiql"
 import "graphiql/graphiql.css"
@@ -32,9 +32,49 @@ const fetcher = async (graphQLParams: any) => {
 }
 
 const SkillsPage = () => {
+  const graphiqlRef = useRef<typeof GraphiQL>()
+  const prettifyRef = useRef<HTMLDivElement>()
+
+  const handleClickPrettifyButton = () => {
+    const editor = graphiqlRef.current.getQueryEditor()
+    const currentText = editor.getValue()
+    const { parse, print } = require("graphql")
+    const prettyText = print(parse(currentText))
+    editor.setValue(prettyText)
+  }
+
+  useEffect(() => {
+    const editor = document.querySelector("#gatsby-focus-wrapper div.queryWrap")!
+
+    // @ts-ignore ignoring since typescript as of this moment doesn't have
+    const resizeObserver: any = new ResizeObserver(() => {
+      if (!prettifyRef || !prettifyRef.current)
+        return
+
+      const prettifyBtnWidth = prettifyRef.current!.getBoundingClientRect().width
+      const editorWidth = editor.getBoundingClientRect().width
+      const padding = 10
+      prettifyRef.current!.style.left = `${editorWidth - prettifyBtnWidth - padding}px`
+    })
+
+    resizeObserver.observe(editor)
+
+    return resizeObserver.disconnect
+  }, [prettifyRef])
+
   return (
     <div className="h-screen w-screen">
-      <GraphiQL style={{ height: "100vh" }} query='' fetcher={fetcher} />
+      <GraphiQL ref={graphiqlRef} style={{ height: "100vh" }} query='' fetcher={fetcher}>
+        <GraphiQL.Toolbar>
+          <div ref={prettifyRef as any} className="custom-prettify-button">
+            <GraphiQL.Button
+              onClick={handleClickPrettifyButton}
+              label="{ } Prettify"
+              title="Prettify Query (Shift-Ctrl-P)"
+            />
+          </div>
+        </GraphiQL.Toolbar>
+      </GraphiQL>
     </div>
   )
 }
