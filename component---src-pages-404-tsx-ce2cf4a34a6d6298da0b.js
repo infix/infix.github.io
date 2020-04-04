aed940b5f70a743e35ff29a130341621b4801b4c
Amr Abulseoud <amrabulseoud@gmail.com>
2020-04-04 17:37:07 +0200

    Updates

---
+++ component---src-pages-404-tsx-ce2cf4a34a6d6298da0b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{"i6+/":function(t,e,n){"use strict";n.r(e);var c=n("q1tI"),u=n.n(c),o=n("H8eV"),s=n("qKvR");e.default=function(){return Object(s.c)(u.a.Fragment,null,Object(s.c)(o.a,{title:"404: Not found"}),Object(s.c)("h1",null,"NOT FOUND"),Object(s.c)("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-tsx-ce2cf4a34a6d6298da0b.js.map
